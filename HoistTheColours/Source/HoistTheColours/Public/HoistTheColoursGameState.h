6a1393937dea67a108bef0f96d94d4be25863399
skyasm1 <skhky316@gmail.com>
2026-08-15 16:16:52 +0900

    template

---
+++ HoistTheColours/Source/HoistTheColours/Public/HoistTheColoursGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "HoistTheColoursGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOISTTHECOLOURS_API AHoistTheColoursGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ HoistTheColours/Source/HoistTheColours/Public/HoistTheColoursPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "HoistTheColoursPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOISTTHECOLOURS_API AHoistTheColoursPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
